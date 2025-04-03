1e95dcbab5ff887c0a9d57e2cecc3dc431d689ee
Zhuo Cheng <maxbeyondmax@gmail.com>
2025-04-03 22:18:09 +0000

    better print

--- tests/unittest/attention/test_gpt_attention.py
+++ tests/unittest/attention/test_gpt_attention.py
@@ -812,22 +812,34 @@ class TestFunctional:
             time_ns = start.elapsed_time(end) * 1e6
 
             print(
-                "dbg in_len, head_size, num_heads, num_kv_heads, batch_size, dtype",
+                "in_len",
                 in_len,
+                "head_size",
                 head_size,
+                "num_heads",
                 num_heads,
+                "num_kv_heads",
                 num_kv_heads,
+                "batch_size",
                 batch_size,
+                "dtype",
                 dtype,
             )
 
-            macs = (in_len * head_size * 2) * (num_heads) * batch_size * 2
-            tflops = int(macs // time_ns // 1000)
-            mem_size = (in_len * head_size) * (num_kv_heads) * batch_size * 2
-            mem_size *= torch.finfo(str_dtype_to_torch(dtype)).bits // 8
-            bw_gbs = mem_size // time_ns
+            if step < generation_step:
+                macs = num_heads * (in_len * in_len * head_size) * 2
+                tflops = macs // time_ns // 1000
+                print(f"Time taken: {time_ns // 1000} us, TFLOPS: {tflops}")
+            else:
+                macs = (in_len * head_size * 2) * (num_heads) * batch_size * 2
+                tflops = int(macs // time_ns // 1000)
+                mem_size = (in_len * head_size) * (num_kv_heads) * batch_size * 2
+                mem_size *= torch.finfo(str_dtype_to_torch(dtype)).bits // 8
+                bw_gbs = mem_size // time_ns
 
-            print(f"Time taken: {time_ns // 1000} us, TFLOPS: {tflops}, BW: {bw_gbs} GB/s")
+                print(
+                    f"Time taken: {time_ns // 1000} us, TFLOPS: {tflops}, BW: {bw_gbs} GB/s, tflops: {tflops}, mem_size: {mem_size} bytes"
+                )
 
             torch.cuda.synchronize()
             return session, outputs['output'], past_key_value
@@ -1235,7 +1247,11 @@ class TestFunctional:
                 )
                 kv_cache_block_offsets = kv_cache_manager.get_block_offsets(
                     beam_width)
-            if step == 0:
+            generation_step = 1
+            if step == generation_step:
+                del session
+                session = None
+            if step < generation_step:
                 host_request_types = torch.tensor([0] * batch_size,
                                                   dtype=torch.int32)
                 if paged_kv_cache:
@@ -1371,22 +1387,42 @@ class TestFunctional:
                                      dtype=str_dtype_to_torch(dtype),
                                      device='cuda')
 
-                print("before prefill")
-                # session, output, present_key_value = _construct_execution(
-                #     session, input_tensor, weight_plugin, bias_plugin,
-                #     present_key_value, kv_cache_block_offsets,
-                #     host_kv_cache_pool_pointers, host_kv_cache_pool_mapping,
-                #     attention_packed_mask, sequence_length,
-                #     host_past_key_value_lengths,
-                #     host_max_attention_window_sizes, host_sink_token_length,
-                #     input_lengths, host_context_lengths, cache_indirection,
-                #     host_request_types, num_heads, hidden_size, num_kv_heads,
-                #     output, dtype, position_embedding_type, max_context_length,
-                #     shape_dict, kv_quant_scale, kv_dequant_scale, configuration,
-                #     context_host_runtime_perf_knobs, host_context_progress)
-                # del session
-                # session = None
-                print("after prefill")
+                print("before prefill", step)
+                session, output, present_key_value = _construct_execution(
+                    session,
+                    input_tensor,
+                    weight_plugin,
+                    bias_plugin,
+                    present_key_value,
+                    kv_cache_block_offsets,
+                    host_kv_cache_pool_pointers,
+                    host_kv_cache_pool_mapping,
+                    attention_packed_mask,
+                    sequence_length,
+                    host_past_key_value_lengths,
+                    host_max_attention_window_sizes,
+                    host_sink_token_length,
+                    input_lengths,
+                    host_context_lengths,
+                    cache_indirection,
+                    host_request_types,
+                    num_heads,
+                    hidden_size,
+                    num_kv_heads,
+                    output,
+                    dtype,
+                    position_embedding_type,
+                    max_context_length,
+                    shape_dict,
+                    kv_quant_scale,
+                    kv_dequant_scale,
+                    configuration,
+                    context_host_runtime_perf_knobs,
+                    host_context_progress,
+                )
+                del session
+                session = None
+                print("after prefill\n\n")
 
                 # if enable_remove_input_padding:
                 #     torch_output = remove_input_padding(torch_output)
